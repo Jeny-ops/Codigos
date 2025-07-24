d3278f6b9a0c7e022740758ce28d45f5ac28123e
Jeny-ops <jenyfferwmedeiros@gmail.com>
2025-07-24 08:31:46 -0300

    Update Ativ 10 BD.sql

--- Ativ 10 BD.sql
+++ Ativ 10 BD.sql
@@ -1,1 +1,31 @@
+create database empresaDeDesenvolvimento;
+use empreseDeDesenvolvimento;
 
+create table linguagemDeProgramacao (
+nome varchar(30) primary key,
+multiplataforma varchar(30),
+aberta unique  
+);
+
+create table empregado (
+cpf int primary key,
+nome varchar(30)  
+);
+
+create table setor(
+nome char(30),
+codigo_setor int primary key  
+);
+
+create table programador (
+especialidade char(30)  
+);
+
+create table engenheiro(
+numero_de_registro int 
+);
+
+create table projeto (
+codigo_projeto int primary key,
+nome_projeto char(30)  
+);
