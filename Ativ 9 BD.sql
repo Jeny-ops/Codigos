90fc52ffa90c5ee5f70d5ddbcbaa639e23f29a31
Jeny-ops <jenyfferwmedeiros@gmail.com>
2025-07-24 10:20:39 -0300

    Update Ativ 9 BD.sql

--- Ativ 9 BD.sql
+++ Ativ 9 BD.sql
@@ -11,16 +11,14 @@ create table professor(
 matricula_professor int primary key,
 nome char (30),
 titulacao char(30),
-codigoDoCampus int not null unique,
+codigoDoCampus int not null,
 foreign key (codigoDoCampus) references campus(codigo)
 );
 
 create table Notebook(
 codigo_notebook int,
 data_compra date,
-profMatricula int not null,
-primary key(codigo_notebook,profMatricula)
-foreign key profMatricula references professor(matricula_professor)
+primary key(codigo_notebook)
 );
 
 create table turma(
@@ -31,15 +29,19 @@ semestre int
 create table aluno(
 matricula_aluno char(30) primary key,
 nome varchar (30),
-rendimento int
+rendimento int,
+codCampus char(30),
+codTurmas varchar(30) not null,
+foreign key (codCampus) references campus(codigo_campus),
+foreign key (codTurmas) references turma(codigo_turma)
 );
 
 -- relacionamentos:
 create table professor_aquisicao_notebook(
 dataAquisicao date,
-professorMatricula int not null,
+professorMatricula int not null unique,
 notebookCodigo int not null,
-primary key(notebookCodigo,professorMatricula),
+primary key(notebookCodigo),
 foreign key (professorMatricula) references professor(matricula_professor),
 foreign key (notebookCodigo) references NoteBook(codigo_notebook)
 );
@@ -47,33 +49,11 @@ foreign key (notebookCodigo) references NoteBook(codigo_notebook)
 create table professor_ensina_turma (
 matriculaProf int not null,
 codigoTurma int not null,
-primary key (codigoTurma),
+primary key (codigoTurma,matriculaProf),
 foreign key (matriculaProf) references professor(matricula_professot),
 foreign key (codigoTurma ) references turma(codigo_turma)
 );
 
-create table professor_e_campus(
-matriculaProf int not null,
-codCampus varchar(30) not null,
-primary key (codCampus),
-foreign key (matriculaProf) references professor(matricula_professor)
-foreign key (codCampus) references campus(codigo_campus)
-);
-
-create table alunos_pertencem_turma(
-matriculasAlunos varchar(30) not null,
-codTurmas varchar(30) not null,
-primary key (codTurmas),
-foreign key (matriculasAlunos) references alunos(matriculas_alunos),
-foreign key (codTurmas) references turma(codigo_turma)
-);
-
-create table campus_e_alunos (
-codCampus varchar(30) not null,
-matriculasAlunos varchar(30) not null,
-primary key (codCampus),
-foreign key (matriculasAlunos) references alunos(matriculas_alunos),
-foreign key (codCampus) references campus(codigo_campus)
 
 
-);
+
