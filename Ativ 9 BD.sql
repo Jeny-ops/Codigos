8e259bf4c4fbed55e511b84c3a22eaedfafb14bc
Jeny-ops <jenyfferwmedeiros@gmail.com>
2025-07-24 08:59:06 -0300

    Update Ativ 9 BD.sql

--- Ativ 9 BD.sql
+++ Ativ 9 BD.sql
@@ -1,15 +1,26 @@
 create database Escola;
 use Escola;
-create table Notebook(
-codigo_notebook char (30) primary key,
-data_compra date
+
+create table campus(
+codigo_campus char (30) primary key,
+endereco varchar (30) not null,
+cidade varchar (30)
 );
+
 create table professor(
 matricula_professor int primary key,
 nome char (30),
 titulacao char(30),
 codigoDoCampus int not null unique,
 foreign key (codigoDoCampus) references campus(codigo)
+);
+
+create table Notebook(
+codigo_notebook int,
+data_compra date,
+profMatricula int not null,
+primary key(codigo_notebook,profMatricula)
+foreign key profMatricula references professor(matricula_professor)
 );
 
 create table turma(
@@ -23,17 +34,12 @@ nome varchar (30),
 rendimento int
 );
 
-create table campus(
-codigo_campus char (30) primary key,
-endereco varchar (30) not null,
-cidade varchar (30)
-);
 -- relacionamentos:
 create table professor_aquisicao_notebook(
 dataAquisicao date,
-professorMatricula int not null unique,
-notebookCodigo char (30) not null ,
-primary key(notebookCodigo),
+professorMatricula int not null,
+notebookCodigo int not null,
+primary key(notebookCodigo,professorMatricula),
 foreign key (professorMatricula) references professor(matricula_professor),
 foreign key (notebookCodigo) references NoteBook(codigo_notebook)
 );
@@ -41,7 +47,7 @@ foreign key (notebookCodigo) references NoteBook(codigo_notebook)
 create table professor_ensina_turma (
 matriculaProf int not null,
 codigoTurma int not null,
-primary key (matriculaProf,codigoTurma),
+primary key (codigoTurma),
 foreign key (matriculaProf) references professor(matricula_professot),
 foreign key (codigoTurma ) references turma(codigo_turma)
 );
@@ -49,7 +55,7 @@ foreign key (codigoTurma ) references turma(codigo_turma)
 create table professor_e_campus(
 matriculaProf int not null,
 codCampus varchar(30) not null,
-primary key (codCampus,matriculaProf),
+primary key (codCampus),
 foreign key (matriculaProf) references professor(matricula_professor)
 foreign key (codCampus) references campus(codigo_campus)
 );
@@ -65,7 +71,7 @@ foreign key (codTurmas) references turma(codigo_turma)
 create table campus_e_alunos (
 codCampus varchar(30) not null,
 matriculasAlunos varchar(30) not null,
-primary key (matriculasAlunos),
+primary key (codCampus),
 foreign key (matriculasAlunos) references alunos(matriculas_alunos),
 foreign key (codCampus) references campus(codigo_campus)
 
