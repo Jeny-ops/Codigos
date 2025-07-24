d63a66bc8f062e2bc14eaa0bcfd5af65762f7715
Jeny-ops <jenyfferwmedeiros@gmail.com>
2025-07-24 08:13:46 -0300

    Update Ativ tab 9 atualizada.sql

--- Ativ tab 9 atualizada.sql
+++ Ativ tab 9 atualizada.sql
@@ -1,5 +1,5 @@
-create database codigo;
-use codigo;
+create database Escola;
+use Escola;
 create table Notebook(
 codigo_notebook char (30) primary key,
 data_compra date
@@ -8,12 +8,12 @@ create table professor(
 matricula_professor int primary key,
 nome char (30),
 titulacao char(30),
-codigoDoCampus int not null,
+codigoDoCampus int not null unique,
 foreign key (codigoDoCampus) references campus(codigo)
 );
 
 create table turma(
-codigo_turma char(30) not null primary key,
+codigo_turma char(30) primary key,
 semestre int
 );
 
@@ -32,11 +32,12 @@ cidade varchar (30)
 create table professor_aquisicao_notebook(
 dataAquisicao date,
 professorMatricula int not null unique,
-notebookCodigo char (30) not null,
+notebookCodigo char (30) not null ,
 primary key(notebookCodigo),
 foreign key (professorMatricula) references professor(matricula_professor),
-foreign key (notebookCodigo) references professor(codigo_notebook)
+foreign key (notebookCodigo) references NoteBook(codigo_notebook)
 );
+
 create table professor_ensina_turma (
 matriculaProf int not null,
 codigoTurma int not null,
@@ -48,12 +49,13 @@ foreign key (codigoTurma ) references turma(codigo_turma)
 create table professor_e_campus(
 matriculaProf int not null,
 codCampus varchar(30) not null,
-
+primary key (codCampus,matriculaProf),
 foreign key (matriculaProf) references professor(matricula_professor)
+foreign key (codCampus) references campus(codigo_campus)
 );
 
 create table alunos_pertencem_turma(
-matriculasAlunos varchar(30) not null unique,
+matriculasAlunos varchar(30) not null,
 codTurmas varchar(30) not null,
 primary key (codTurmas),
 foreign key (matriculasAlunos) references alunos(matriculas_alunos),
@@ -61,7 +63,7 @@ foreign key (codTurmas) references turma(codigo_turma)
 );
 
 create table campus_e_alunos (
-codCampus varchar(30) not null unique,
+codCampus varchar(30) not null,
 matriculasAlunos varchar(30) not null,
 primary key (matriculasAlunos),
 foreign key (matriculasAlunos) references alunos(matriculas_alunos),
