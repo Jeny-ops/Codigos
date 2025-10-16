2c82dffcc148cf30ff332da5ff1d0265ea4e86c3
Jeny-ops <jenyfferwmedeiros@gmail.com>
2025-10-16 09:18:08 -0300

    Implement paint calculation logic in q9.java
    Refactor paint store program to calculate required paint cans and total cost.

--- ativJavaBd/q9.java
+++ ativJavaBd/q9.java
@@ -1,51 +1,27 @@
-// nao concluida!!
 import java.util.Scanner;
 
 public class q9 {
     public static void main(String[] args) {
-      
-      /*Faça um programa para uma loja de tintas. O programa deverá pedir o tamanho em metros
-quadrados da área a ser pintada. Considere que a cobertura da tinta é de 1 litro para cada 3 metros
-quadrados e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00. Informe ao usuário
-a quantidades de latas de tinta a serem compradas e o preço total.*/
-        
-        Scanner ler = new Scanner (System.in);
-        int area;
-        int litrosTinta=0;
-        int quantLatas;
-        double precoTotal;
-        
-        
-        
-        System.out.println("Tamanho em metros quadrados da área a ser pintada: " );
-        area=ler.nextInt();
-        
-        litrosTinta = litrosTinta + area/3;
-       
-        if(area>54){
-            litrosTinta++;
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Área que será pintada(m²):");
+        double area = ler.nextDouble();
+
+        ler.close();
+        double litros = area/3;
+        double latas = litros / 18;
+
+        if((latas - (int)latas) != 0 ){
+            latas = (int)latas + 1;
         }
-        
-        quantLatas= litrosTinta/18;
-        
-        
-        if(quantLatas%18==0 || quantLatas%18==1){
-            
-            quantLatas=1;
-            
-        }
-       
-       while(quantLatas%18!=0 || quantLatas%18!=1){
-           
-           quantLatas=1;
-           
-           quantLatas=quantLatas+1;
-           
-       }
-        
-        System.out.println("Quantidades de latas de tinta a serem compradas: " + quantLatas);
-        
-        
-        
+
+        double valor = latas * 80;
+
+        System.out.println("Quantidade de latas necessárias: "+latas);
+
+        System.out.println("Valor total: "+ valor);
+
+
+
     }
 }
