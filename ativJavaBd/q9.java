36affae36719d104669ca58957b6003da91734ef
Jeny-ops <jenyfferwmedeiros@gmail.com>
2025-10-16 08:59:34 -0300

    Update q9.java

--- ativJavaBd/q9.java
+++ ativJavaBd/q9.java
@@ -1,27 +1,49 @@
 // nao concluida!!
 import java.util.Scanner;
 
-public class q8 {
+public class q9 {
     public static void main(String[] args) {
       
       /*Faça um programa para uma loja de tintas. O programa deverá pedir o tamanho em metros
 quadrados da área a ser pintada. Considere que a cobertura da tinta é de 1 litro para cada 3 metros
 quadrados e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00. Informe ao usuário
 a quantidades de latas de tinta a serem compradas e o preço total.*/
+        
         Scanner ler = new Scanner (System.in);
-        double area;
-        int areaPor3;
-        int quantintas;
-        int quantBaldes;
+        int area;
+        int litrosTinta=0;
+        int quantLatas;
         double precoTotal;
         
         
-        area = ler.nextDouble();
-        System.out.println("tamanho em metros quadrados da área a ser pintada: " + area);
         
-        areaPor3= area/3;
-        quantBaldes= quantintas/3;
-        quantintas= area - quantBaldes;
+        System.out.println("Tamanho em metros quadrados da área a ser pintada: " );
+        area=ler.nextInt();
+        
+        litrosTinta = litrosTinta + area/3;
+       
+        if(area>54){
+            litrosTinta++;
+        }
+        
+        quantLatas= litrosTinta/18;
+        
+        
+        if(quantLatas%18==0 || quantLatas%18==1){
+            
+            quantLatas=1;
+            
+        }
+       
+       while(quantLatas%18!=0 || quantLatas%18!=1){
+           
+           quantLatas=1;
+           
+           quantLatas=quantLatas+1;
+           
+       }
+        
+        System.out.println("Quantidades de latas de tinta a serem compradas: " + quantLatas);
         
         
         
