dfdb1143ed851e02e9e808d1354c922d674d3f9e
Jeny-ops <jenyfferwmedeiros@gmail.com>
2025-10-21 08:22:40 -0300

    Create q17.java

---
+++ ativJavaBd/q17.java
@@ -0,0 +1,13 @@
+public class q17{
+
+  public static void main (String [] args){
+    String [] produtos = new String [5];
+    produtos[0]= "Cachorro-quente";
+    produtos[1]= "Bauru simples";
+    
+
+    
+  }
+
+  
+}
