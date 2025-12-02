3d78dd3bed549c7754351b676214653ddda2cada
Jeny-ops <jenyfferwmedeiros@gmail.com>
2025-12-02 09:32:25 -0300

    Update q2.java

--- lista_4bimBD/q2.java
+++ lista_4bimBD/q2.java
@@ -15,47 +15,44 @@ public class consultarAluno {
         String sql = "select * from dadosAluno";
 
         try (Connection conexao = DriverManager.getConnection(url, usuario, senha);
-                PreparedStatement stmt = conexao.prepareStatement(sql);
-                ResultSet rs = stmt.executeQuery()) {
+             PreparedStatement stmt = conexao.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
 
-            if (rs.isBeforeFirst()) {
-                while (rs.next()) {
-                    var id = rs.getInt("numero");
-                    var nome = rs.getString("nome");
-                    var curso = rs.getString("curso");
-                    var nota1 = rs.getDouble("nota1");
-                    var nota2 = rs.getDouble("nota2");
-                    var nota3 = rs.getDouble("nota3");
-                    var nota4 = rs.getDouble("nota4");
-                    var media = (nota1 + nota2 + nota3 + nota4)/4;
-                    var situacao;
-
-                    if(media>=7){
-                        situacao="aprovado";
-                    }
-
-                    else if(media<3){
-                        situacao="reprovado";
-                    } else{
-                        situacao="recuperação";
-                    }
-
-
-                    System.out.println("Aluno, " +  id + ": "nome);
-                    System.out.println("curso: " + curso);
-                    System.out.println("notas: " + nota1 ", " + nota2 + ", " + nota3 + ", " + nota4);
-                    System.out.println("situação do aluno: " + situacao + "com média:" + media );
-
-                    System.out.println("------------------------");
-                }
-            } else {
+            if (!rs.isBeforeFirst()) {  // verifica se há registros
                 System.out.println("Nenhum aluno cadastrado!");
+                return;
+            }
+
+            while (rs.next()) {
+                var numero = rs.getInt("numero");
+                var nome = rs.getString("nome");
+                var curso = rs.getString("curso");
+                var nota1 = rs.getDouble("nota1");
+                var nota2 = rs.getDouble("nota2");
+                var nota3 = rs.getDouble("nota3");
+                var nota4 = rs.getDouble("nota4");
+
+                var media = (nota1 + nota2 + nota3 + nota4) / 4.0;
+
+                String situacao;
+
+                if (media >= 7) {
+                    situacao = "Aprovado";
+                } else if (media < 3) {
+                    situacao = "Reprovado";
+                } else {
+                    situacao = "Recuperação";
+                }
+
+                System.out.println("Aluno " + numero + ": " + nome);
+                System.out.println("Curso: " + curso);
+                System.out.println("Notas: " + nota1 + " " + nota2 + " " + nota3 + " " + nota4);
+                System.out.println("Situação: " + situacao + " com média " + media);
+                System.out.println("-------------------------------------");
             }
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
     }
-
 }
